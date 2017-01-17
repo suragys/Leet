067c1308e2b3f766f55e0bdc74e8ed2f93ad9df2
suragys <suragys@gmail.com>
2017-01-17 13:54:37 -0800

    Create 485MaxConsecutiveOnes.java
    Given a binary array, find the maximum number of consecutive 1s in this array.
    
    Example 1:
    Input: [1,1,0,1,1,1]
    Output: 3
    Explanation: The first two digits or the last three digits are consecutive 1s.
        The maximum number of consecutive 1s is 3.
    Note:
    
    The input array will only contain 0 and 1.
    The length of input array is a positive integer and will not exceed 10,000

---
+++ src/com/leetcode/easy/485MaxConsecutiveOnes.java
@@ -0,0 +1,32 @@
+/*
+Given a binary array, find the maximum number of consecutive 1s in this array.
+
+Example 1:
+Input: [1,1,0,1,1,1]
+Output: 3
+Explanation: The first two digits or the last three digits are consecutive 1s.
+    The maximum number of consecutive 1s is 3.
+Note:
+
+The input array will only contain 0 and 1.
+The length of input array is a positive integer and will not exceed 10,000
+*/
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int max = 0;
+        int count = 0;
+        // System.out.println(nums.length);
+        for(int i = 0; i < nums.length; i++){
+            // System.out.println(nums[i]);
+            if(nums[i] == 1){
+                count++;
+            } else{
+                if(max < count){
+                    max = count;
+                }
+                count = 0;
+            }
+        }
+        return max > count ? max : count;
+    }
+}
